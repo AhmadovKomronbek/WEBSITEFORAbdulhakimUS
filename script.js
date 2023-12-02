85020f6b06ab9ae607a0cde3ce4b6c5532e488a0
Komronbek <138858973+AhmadovKomronbek@users.noreply.github.com>
2023-12-02 18:38:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $(".spinner-box").addClass("d-none");
+    $(".body").removeClass("d-none");
+    // setTimeout( function(){
+    //     $(".spinner-box").addClass("d-none");
+    //     $(".body").removeClass("d-none");
+    // }, 1500);
+});
+
+// ScrollTop
+
+window.addEventListener("scroll", function(){
+    document.querySelector(".scrollTop").classList.toggle("scrolltopactiv", window.scrollY > 400);
+});
+
+// ScrollTop
